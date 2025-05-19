8e8de995a58b354a0c4675eeb2937ebae956551d
Gomer771 <swistak@tutamail.com>
2025-05-19 13:19:06 +0300

    Update cod.py

--- cod.py
+++ cod.py
@@ -1,12 +1,3 @@
-import pandas as pd
 
-class Terminal:
-    def __init__(self):
-        filename = 'var6.csv' 
-        try:
-            with open(filename, encoding='utf-8') as file:
-                self.contents = file.read()
-        except FileNotFoundError:
-            print(f"Возникла следующая ошибка: [Errno 2] No such file or directory: '{filename}'")
 
 
